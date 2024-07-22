cb4e38fa06dcbe4560dc00b3c97a7ac136d0715e
Brian Connolly <brian.connolly71@gmail.com>
2024-07-22 20:44:34 +0100

    added favicon setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,27 @@
 import React from 'react'
-import logo from '../assets/kevinRushLogo.png'
+import logo from '../assets/Brian_Connolly_logo.png'
 import { FaLinkedin, FaGithub, FaMedium, FaTwitterSquare } from 'react-icons/fa'
 
 const Navbar = () => {
   return (
-    <nav className="bg-red-900 mb-20 flex items-center justify-between py-6">
-        <div className="flex flex-shrink-0 items-center">
-            <img src={logo} alt="Brian connolly.dev logo" />
+    <div className="navbar bg-base-100">
+        <div className="navbar-start">
+            <img src={logo} width={150} className alt="Brian connolly.dev logo" />
         </div>
-        <div className="m-8 flex items-center justify-center gap-5 text-2xl">
+        <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">
+            <li><a>Item 1</a></li>
+            <li><a>Item 2</a></li>
+            <li><a>Item 3</a></li>
+            </ul>
+        </div>
+        <div className="navbar-end gap-5 text-2xl p-3">
             <FaLinkedin />
             <FaGithub />
             <FaMedium />
             <FaTwitterSquare />
         </div>
-        <div>
-        <button class="btn">Hello daisyUI</button>
-        </div>
-    </nav>
+    </div>
   )
 }
 
