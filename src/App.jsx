7ec764a8e127b45932084e7c8525cbdf4d53a97f
Brian Connolly <brian.connolly71@gmail.com>
2024-07-23 18:44:50 +0100

    setup barebones of components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import About from './components/About'
+import Experience from './components/Experience'
+import Education from './components/Education'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="overflow-x-hidden container mx-auto px-8">
     <Navbar />
     <Hero />
+    <About />
+    <Experience />
+    <Education />
+    <Footer />
     </div>
   )
 }
