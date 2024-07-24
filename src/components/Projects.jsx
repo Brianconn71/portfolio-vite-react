54ec202313bb2fcf5699f996e2529a0bdd8017cb
Brian Connolly <brian.connolly71@gmail.com>
2024-07-24 18:07:48 +0100

    ADD: added functionality for experiences

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,20 +3,7 @@ import React from 'react'
 const Projects = () => {
   return (
     <div id="projects" className="container">
-        <div className="card lg:card-side bg-base-100 shadow-xl">
-    <figure>
-        <img
-        src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
-        alt="Album" />
-    </figure>
-    <div className="card-body">
-        <h2 className="card-title">New album is released!</h2>
-        <p>Click the button to listen on Spotiwhy app.</p>
-        <div className="card-actions justify-end">
-        <button className="btn btn-primary">Listen</button>
-        </div>
-    </div>
-        </div>
+      <h2>Projects</h2>
     </div>
   )
 }
