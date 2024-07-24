a814d651a050be666af3ea4c8375b39243dc8bce
Brian Connolly <brian.connolly71@gmail.com>
2024-07-24 18:23:13 +0100

    updated page layout asnd background color

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div id="projects" className="container">
+    <div id="projects" className="container bg-base-300">
       <h2>Projects</h2>
     </div>
   )
