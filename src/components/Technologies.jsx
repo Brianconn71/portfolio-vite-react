b987687ba2ee853bc6d287833a81a0ce8a002386
Brian Connolly <brian.connolly71@gmail.com>
2024-07-24 19:47:23 +0100

    add: added correct styling and setup to technologies

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -6,52 +6,55 @@ import { BiLogoPostgresql } from "react-icons/bi";
 
 const Technologies = () => {
   return (
-    <div id="technologies" className="container mx-auto border-b bg-base-200 border-base-300 pb-24">
-      <h2 className="my-20 text-center text-4xl">Technologies</h2>
-      <div className="flex flex-wrap items-center justify-center gap-4">
-        <div className="rounded-2xl border-2 border-base-100 p-4">
-            <SiReact className="text-7xl"/>
+    <div id="technologies" className="border-b bg-neutral-content border-base-300 pb-24">
+        <h2 className="my-20 text-center text-4xl">Technologies</h2>
+        <div className="container mx-auto ">
+            <div className="flex flex-wrap items-center justify-center gap-4">
+                <div className="rounded-2xl border-2 border-base-300 p-4">
+                    <SiReact className="text-7xl"/>
+                </div>
+                <div className="rounded-2xl border-2 border-base-300 p-4">
+                    <RiNextjsFill className="text-7xl"/>
+                </div>
+                <div className="rounded-2xl border-2 border-base-300 p-4">
+                    <SiDjango className="text-7xl"/>
+                </div>
+                <div className="rounded-2xl border-2 border-base-300 p-4">
+                    <SiMongodb className="text-7xl"/>
+                </div>
+                <div className="rounded-2xl border-2 border-base-300 p-4">
+                    <BiLogoPostgresql className="text-7xl"/>
+                </div>
+                <div className="rounded-2xl border-2 border-base-300 p-4">
+                    <FaPython className="text-7xl"/>
+                </div>
+                <div className="rounded-2xl border-2 border-base-300 p-4">
+                    <FaAws className="text-7xl"/>
+                </div>
+                <div className="rounded-2xl border-2 border-base-300 p-4">
+                    <SiJavascript className="text-7xl"/>
+                </div>
+                <div className="rounded-2xl border-2 border-base-300 p-4">
+                    <SiTypescript className="text-7xl"/>
+                </div>
+                <div className="rounded-2xl border-2 border-base-300 p-4">
+                    <FaPhp className="text-7xl"/>
+                </div>
+                <div className="rounded-2xl border-2 border-base-300 p-4">
+                    <FaDocker className="text-7xl"/>
+                </div>
+                <div className="rounded-2xl border-2 border-base-300 p-4">
+                    <FaJenkins className="text-7xl"/>
+                </div>
+                <div className="rounded-2xl border-2 border-base-300 p-4">
+                    <SiFastapi className="text-7xl"/>
+                </div>
+                <div className="rounded-2xl border-2 border-base-300 p-4">
+                    <SiStreamlit className="text-7xl"/>
+                </div>
+            </div>
         </div>
-        <div className="rounded-2xl border-2 border-base-100 p-4">
-            <RiNextjsFill className="text-7xl"/>
-        </div>
-        <div className="rounded-2xl border-2 border-base-100 p-4">
-            <SiDjango className="text-7xl"/>
-        </div>
-        <div className="rounded-2xl border-2 border-base-100 p-4">
-            <SiMongodb className="text-7xl"/>
-        </div>
-        <div className="rounded-2xl border-2 border-base-100 p-4">
-            <BiLogoPostgresql className="text-7xl"/>
-        </div>
-        <div className="rounded-2xl border-2 border-base-100 p-4">
-            <FaPython className="text-7xl"/>
-        </div>
-        <div className="rounded-2xl border-2 border-base-100 p-4">
-            <FaAws className="text-7xl"/>
-        </div>
-        <div className="rounded-2xl border-2 border-base-100 p-4">
-            <SiJavascript className="text-7xl"/>
-        </div>
-        <div className="rounded-2xl border-2 border-base-100 p-4">
-            <SiTypescript className="text-7xl"/>
-        </div>
-        <div className="rounded-2xl border-2 border-base-100 p-4">
-            <FaPhp className="text-7xl"/>
-        </div>
-        <div className="rounded-2xl border-2 border-base-100 p-4">
-            <FaDocker className="text-7xl"/>
-        </div>
-        <div className="rounded-2xl border-2 border-base-100 p-4">
-            <FaJenkins className="text-7xl"/>
-        </div>
-        <div className="rounded-2xl border-2 border-base-100 p-4">
-            <SiFastapi className="text-7xl"/>
-        </div>
-        <div className="rounded-2xl border-2 border-base-100 p-4">
-            <SiStreamlit className="text-7xl"/>
-        </div>
-      </div>
+        
     </div>
   )
 }
