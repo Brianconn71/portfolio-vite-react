d756512b49075dc2f63af6aa30662bbb57f00649
Brian Connolly <brian.connolly71@gmail.com>
2024-07-25 23:21:50 +0100

    EDIT: changed about text

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,65 +3,58 @@ import project2 from "../assets/images/olympic_data.png";
 import project3 from "../assets/images/dunmore.png";
 import project4 from "../assets/images/townley.png";
 
-export const HERO_CONTENT = `I am an enthusiastic and passionate Software Engineer with a desire to build highly scalable Infrastructure and applications. I have a wide range of experience in Fronttend development, Backend Development and Full-Stack. I have embarked on a journey to understand and use Statistics and Machine Learning principles to understand data and to help influence decisions.With 3 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My biggest Passion lies in Python and the wide range of applications that can be built using various python frameworks such as Django, Flask, FastApi and Streamlit. I have completed a Masters degree in Cloud Computing in which I received a first class honours Grade. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `Welcome to my Portfolio. I am a passionate Software Engineer who has a great love for data and harnessing the power of Machine Learning and AI to develop cool new things! If you have some time (less than two minutes!) then please watch my introduction below and feel free to download my CV.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am an enthusiastic and passionate Software Engineer with a desire to build highly scalable Infrastructure and applications. I have a wide range of experience in Frontend development, Backend Development and Full-Stack. I have embarked on a journey to understand and use Statistics and Machine Learning principles to understand data and to help influence decisions.With 3 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My biggest Passion lies in Python and the wide range of applications that can be built using various python frameworks such as Django, Flask, FastApi and Streamlit. I have completed a Masters degree in Cloud Computing in which I received a first class honours Grade. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    year: "2023 - 2024",
+    role: "Software Engineer",
+    company: "Ezora Ltd.",
+    description: ``,
     technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
   },
   {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    year: "2021 - 2023",
+    role: "Junior Software Engineer",
+    company: "Payslip Ltd.",
+    description: ``,
     technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
   },
   {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    year: "2020 - Present",
+    role: "Freelance Full Stack Software Developer",
+    company: "Freelance",
+    description: ``,
     technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
   },
 ];
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "Refugee Asylum Streamlit Data Dashboard",
     image: project1,
     description:
       "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
     technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
   },
   {
-    title: "Task Management App",
+    title: "Olympic Medals Streamlit Data Dashboard",
     image: project2,
     description:
       "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
     technologies: ["HTML", "CSS", "Angular", "Firebase"],
   },
   {
-    title: "Portfolio Website",
+    title: "Dunmore Medical Website",
     image: project3,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
     technologies: ["HTML", "CSS", "React", "Bootstrap"],
   },
   {
-    title: "Blogging Platform",
+    title: "Townley Medical Website",
     image: project4,
     description:
       "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
