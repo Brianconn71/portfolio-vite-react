bc5398f288e63ce60f48e77bae8121b7c044e16c
Brian Connolly <brian.connolly71@gmail.com>
2024-07-25 19:24:18 +0100

    ADD: finished adding animations to site

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,19 +7,32 @@ import {
   FaTwitterSquare,
 } from "react-icons/fa";
 import { CONTACT } from "../constants";
+import { motion } from "framer-motion";
 
 const Footer = () => {
   return (
     <footer className="footer footer-center bg-secondary text-primary-content p-10">
       <aside>
         <a href="https://brianconnolly.dev">
-          <img src={logo} className="w-40" alt="Brian connolly.dev logo" />
+          <img src={logo} className="w-40" rel="noopener noreferrer" alt="Brian connolly.dev logo" />
         </a>
         <p className="font-bold">
-          <h2 className="text-center font-medium text-4xl">Get in touch</h2>
+          <motion.h2
+          whileInView={{opacity: 1, y: 0}}
+          initial={{opacity: 0, y: -100}}
+          transition={{duration: 1.5}}
+          className="text-center font-medium text-4xl">Get in touch</motion.h2>
           <div className="text-center">
-            <p className="my-4">{CONTACT.address}</p>
-            <p className="my-4">
+            <motion.p
+            whileInView={{opacity: 1, x: 0}}
+            initial={{opacity: 0, x: -100}}
+            transition={{duration: 1}}
+            className="my-4">{CONTACT.address}</motion.p>
+            <motion.p 
+            whileInView={{opacity: 1, x: 0}}
+            initial={{opacity: 0, x: 100}}
+            transition={{duration: 1}}
+            className="my-4">
               <a
                 href="	https://wa.me/353863268858"
                 target="_blank"
@@ -27,12 +40,16 @@ const Footer = () => {
               >
                 {CONTACT.phoneNo}
               </a>
-            </p>
-            <p className="my-4">
-              <a href="mailto:brian@brianconnolly.dev" className="border-b">
+            </motion.p>
+            <motion.p
+            whileInView={{opacity: 1, x: 0}}
+            initial={{opacity: 0, x: -100}}
+            transition={{duration: 1}}
+            className="my-4">
+              <a href="mailto:brian@brianconnolly.dev" rel="noopener noreferrer" className="border-b">
                 {CONTACT.email}
               </a>
-            </p>
+            </motion.p>
           </div>
         </p>
         <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
@@ -41,17 +58,18 @@ const Footer = () => {
         <div className="grid grid-flow-col gap-4 text-3xl">
           <a
             href="https://www.linkedin.com/in/brian-connolly-0b3791b0/"
+            rel="noopener noreferrer"
             target="_blank"
           >
             <FaLinkedin />
           </a>
-          <a href="https://github.com/Brianconn71" target="_blank">
+          <a href="https://github.com/Brianconn71" rel="noopener noreferrer" target="_blank">
             <FaGithub />
           </a>
-          <a href="https://medium.com/@brian.connolly71" target="_blank">
+          <a href="https://medium.com/@brian.connolly71" rel="noopener noreferrer" target="_blank">
             <FaMedium />
           </a>
-          <a href="https://x.com/Brianconnwebdev" target="_blank">
+          <a href="https://x.com/Brianconnwebdev" rel="noopener noreferrer" target="_blank">
             <FaTwitterSquare />
           </a>
         </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -27,7 +27,7 @@ const Projects = () => {
         <div className="flex flex-wrap items-center justify-center gap-4">
           {PROJECTS.map((project, index) => (
             <motion.div
-            variants={projects(1.5)}
+            variants={projects(5)}
             initial="hidden"
             animate="visible"
               key={index}
